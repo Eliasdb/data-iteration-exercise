968a501b95cae1a3f8419597fa0a25959e5469ed
Elias <elias@Eliass-MacBook-Pro.local>
2023-02-16 11:45:52 +0100

    init project

---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+const Title = ({ title, subTitle }: { title: string; subTitle: string }) => {
+  return (
+    <div className="section-title">
+      <h2>
+        {title} <span>{subTitle}</span>
+      </h2>
+    </div>
+  );
+};
+export default Title;
